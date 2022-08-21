b9d2e6d2326062f8d6357d49172294bf4f43a1d5
Nick Navas <npnavas@asu.edu>
2022-08-21 15:27:39 -0700

    Added comments for documentation in test.py

--- Project_1_codes/test.py
+++ Project_1_codes/test.py
@@ -1,10 +1,13 @@
 import simpledecrypt as sd
 
+#Test case for 6
 msg1 = 'PBGLMHGVANKVABEE'
 sd.shift_decrypt(msg1)
-
+#Test case for 5
 msg2 = "AZEPCBBGYYCGXCPXXWPEIWREGYYCGXFZCPXWPE"
 sd.decrypt_affine_key(msg2, [7,2])
-
+#Test cases for 7
 msg3 = 'GNNIMUKSUIPYVANGSUNYPYKUSCNUSBUHLQNYVOUPYKNGOYQPRVYNXGVRGNWGNWGNDIRYA'
 sd.decrypt_affine_no_key(msg3, 3)
+sd.decrypt_affine_key(msg3, [3,8])
+sd.decrypt_affine_key(msg3, [23,11])
