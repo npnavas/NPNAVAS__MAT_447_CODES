798cd2293c9a26f1bdfd89efc3a4534559ad799a
Nick Navas <npnavas@asu.edu>
2022-09-06 03:20:12 -0700

    minot change to P1codes test.py and added P3codes with the my code for 5 and 6

--- Project_1_codes/test.py
+++ Project_1_codes/test.py
@@ -2,12 +2,14 @@ import simpledecrypt as sd
 
 #Test case for 6
 msg1 = 'DBMXLKBLXABZAXLMTZTBGLMMAXPBGWGHMPBMABM - PBGLMHGVANKVABEE'
-sd.shift_decrypt(msg1)
+#sd.shift_decrypt(msg1)
 #Test case for 5
 msg2 = "AZEPCBBGYYCGXCPXXWPEIWREGYYCGXFZCPXWPE"
-sd.decrypt_affine_key(msg2, [7,2])
+#sd.decrypt_affine_key(msg2, [7,2])
 #Test cases for 7
 msg3 = 'GNNIMUKSUIPYVANGSUNYPYKUSCNUSBUHLQNYVOUPYKNGOYQPRVYNXGVRGNWGNWGNDIRYA'
-sd.decrypt_affine_no_key(msg3, 3)
-sd.decrypt_affine_key(msg3, [3,8])
-sd.decrypt_affine_key(msg3, [23,11])
+
+msg4 = "GO"
+sd.decrypt_affine_no_key(msg4, 26)
+sd.decrypt_affine_key(msg4, [5,12])
+#sd.decrypt_affine_key(msg3, [23,11])
---
+++ Project_3_codes/Project_3_README.md
@@ -0,0 +1,2 @@
+Here this notebook is meant to be copy and pasted from here into a Mathematica
+interpreter since I don't know how to run this in the Git Bash shell.
